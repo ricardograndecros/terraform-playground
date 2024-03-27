fec982f7c66a02d3130c49e7e1dedb66d11ea18b
ricardograndecros <ricardograndecros@gmail.com>
2024-03-27 18:27:27 +0100

    04 — Setup vcs and tf files

---
+++ 04_terraform_vcs_integration/main.tf
@@ -0,0 +1,44 @@
+terraform {
+  cloud {
+    organization = "terraform-tutorial-org-rgc"
+    workspaces {
+      name = "learn-tfc-gcp-vcs"
+    }
+  }
+
+  required_providers {
+    google = {
+      source = "hashicorp/google"
+      version = "4.51.0"
+    }
+  }
+}
+
+provider "google" {
+  project = var.GCP_PROJECT_ID
+  region = var.GOOGLE_DEFAULT_REGION
+  zone = var.GOOGLE_DEFAULT_ZONE
+  credentials = var.GOOGLE_CREDENTIALS
+}
+
+resource "google_compute_network" "vpc_network" {
+  name = "terraform-network"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "e2-micro"
+  tags = [ "playground", "test" ]
+
+  boot_disk {
+    initialize_params {
+      image = "cos-cloud/cos-stable"
+    }
+  }
+
+  network_interface {
+    network = google_compute_network.vpc_network.name
+    access_config {
+    }
+  }
+}
---
+++ 04_terraform_vcs_integration/variables.tf
@@ -0,0 +1,11 @@
+variable "GCP_PROJECT_ID" { 
+}
+
+variable "GOOGLE_CREDENTIALS" {
+}
+
+variable "GOOGLE_DEFAULT_REGION" {
+}
+
+variable "GOOGLE_DEFAULT_ZONE" {
+}
